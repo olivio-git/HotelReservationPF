97ec710175b6ef3b84bc4f64cc000c6acc22a89b
olivio-git <subelzaolivitocabezas@gmail.com>
2023-05-26 11:10:09 -0400

    proyecto base

---
+++ HotelReservation/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelReservation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservation
+{
+    public class AppDbContext: DbContext
+    {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+            public DbSet<Habitacion> Habitaciones { get; set; }
+
+    }
+}
---
+++ HotelReservation/Models/Habitacion.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservation.Models
+{
+    public class Habitacion
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Descripcion { get; set; }
+    }
+}
