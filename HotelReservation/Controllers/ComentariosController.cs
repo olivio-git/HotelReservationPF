0afb60f2b8122ed4c2c06ca5679605513f94ab16
charles reinaldo andres jara farfan <tj.charles.jara.f@upds.net.bo>
2023-06-02 16:25:33 -0400

    FAQ-COMMENTS

---
+++ HotelReservation/Controllers/ComentariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
